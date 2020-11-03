168ca0d52fbee3c5abfbbd86e510f48704b1913f
danilopezyejas <lopezyejas@gmail.com>
2020-11-03 20:57:21 -0300

    subir a heroku

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/dist/chat-bot-web'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/chat-bot-web/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8081);
