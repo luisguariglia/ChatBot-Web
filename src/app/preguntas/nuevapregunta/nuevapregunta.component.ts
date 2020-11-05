e82d586da8a60800e6754e8e086f8b7cce2e0db8
mauran190 <maurogonzalezsosa@gmail.com>
2020-11-04 23:07:57 -0300

    Agregada la inclusión de varias formas de hacer una pregunta

--- src/app/preguntas/nuevapregunta/nuevapregunta.component.ts
+++ src/app/preguntas/nuevapregunta/nuevapregunta.component.ts
@@ -15,33 +15,56 @@ export class NuevapreguntaComponent implements OnInit {
     nombre: new FormControl('',[
       Validators.required,
     ]),
-    pregunta: new FormControl('',[
-      Validators.required,
-      ]),
     respuesta: new FormControl('',[
       Validators.required,
       ])
   });
+  secondForm = new FormGroup({
+    pregunta: new FormControl('',[
+      
+      ])
+  });
+  formasPregunta = new Array();
+ 
   constructor(private preguntaService: PreguntaService,
     private fb: FormBuilder,
     private _location: Location,
     private router: Router,
     private toastr:ToastrService) {
       const navigation = this.router.getCurrentNavigation();
+      this.formasPregunta = new Array();
+     
      }
 
   ngOnInit(): void {
   }
+  agregarForma(): void{
+    this.formasPregunta.push(this.secondForm.value.pregunta);
+    
+    this.secondForm.get('pregunta').setValue("");
+    
+  //  console.log(this.formasPregunta);
+  }
+  quitar(): void{
+    this.formasPregunta.pop();
+  }
   addPregunta() : void{
-    this.preguntaService.nuevaPregunta(
-    this.profileForm.value.nombre,
-    this.profileForm.value.pregunta,
-    this.profileForm.value.respuesta
-    ).subscribe(data => {
-      this.toastr.success("Nueva pregunta registrada con éxito");
-      this.router.navigateByUrl('/preguntas');
-
-    });
+   // console.log(this.formasPregunta);
+    
+    if(this.formasPregunta.length == 0){
+      this.toastr.error("No has agregado ninguna forma de preguntar");
+    }else{
+      this.preguntaService.nuevaPregunta(
+        this.profileForm.value.nombre,
+        this.formasPregunta,
+        this.profileForm.value.respuesta
+        ).subscribe(data => {
+          this.toastr.success("Nueva pregunta registrada con éxito");
+          this.router.navigateByUrl('/preguntas');
+    
+        });
+    }
+    
     
 
   }
