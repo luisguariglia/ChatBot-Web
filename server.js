bb294e609fddf852265fa7f90fe8d663e1977d58
mauran190 <maurogonzalezsosa@gmail.com>
2020-11-16 16:42:55 -0300

    ahora los resultados de la wiki se abren en nueva pestaña

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/dist/ChatBotWeb'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname + '/dist/ChatBotWeb/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
